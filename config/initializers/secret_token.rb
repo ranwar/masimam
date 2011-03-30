393baf240a869c5d925a97e3bd5e773127711c6c
Reza Anwar <ranwar@gmail.com>
2011-03-30 22:41:34 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Masimam::Application.config.secret_token = '5e7732ca74b9c18e6a93486b68fcb66f4085b47d6e916e2804a97dcce6fa598f888f4f6fd0e56790b3e34c90cba7a5ebe86b0c9af7f947a67b01d9fe72c3060f'
